32d42432a6bafc6b1f383cb38cf4cb7a1f45321a
Mostakim Roshid <mostakimroshid@gmail.com>
2024-03-14 09:58:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-6xl text-red-50 bg-red-500'>Vite + React</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
